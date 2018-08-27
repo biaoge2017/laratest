faa2918e99e4f51c999db8a85dbd58d4640605e3
dabiaoge <649883823@qq.com>
2018-08-27 15:35:54 +0800

    'create-user-topic-list'

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,8 +5,8 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
-{
+class User extends Authenticatable {
+
     use Notifiable;
 
     /**
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','introduction','avatar',
+        'name', 'email', 'password', 'introduction', 'avatar',
     ];
 
     /**
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function topics() {
+        return $this->hasMany(Topic::class);
+    }
+
 }
